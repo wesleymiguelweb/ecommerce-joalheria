5354618fd669c967318d5f0e27164f798f0c7164
Wesley Miguel <wesley.miguel7@outlook.com>
2026-04-21 18:48:26 -0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,17 @@
 <?php
 
+foreach (['APP_CONFIG_CACHE', 'APP_ROUTES_CACHE', 'APP_EVENTS_CACHE', 'APP_SERVICES_CACHE', 'APP_PACKAGES_CACHE'] as $key) {
+    if (isset($_ENV[$key]) && $_ENV[$key] === '') {
+        unset($_ENV[$key]);
+    }
+    if (isset($_SERVER[$key]) && $_SERVER[$key] === '') {
+        unset($_SERVER[$key]);
+    }
+    if (getenv($key) === '') {
+        putenv($key);
+    }
+}
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
